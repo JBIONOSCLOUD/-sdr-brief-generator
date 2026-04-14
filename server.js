54a4aaa8bb1cb2a4b492a25e88fb9923726317c8
JBIONOSCLOUD <jules.bruguiere@ionos.com>
2026-04-14 18:10:55 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('public'));
+app.use(express.static('.'));
 
 // Servir l'index HTML
 app.get('/', (req, res) => {
